3997ffadbc9e9d5852a26541f649b906e4bc0549
Aditya Anand Thakur <anandaditya444@gmail.com>
2020-08-03 15:56:00 +0530

    Gail pic

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,6 +11,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('gail');
+});
+
+router.get('/index', function(req, res, next) {
   res.render('index');
 });
 
